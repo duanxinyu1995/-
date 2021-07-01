002cd4fdb4bcd263150fec217c64b3b230313490
duanxinyu1995 <1261102301@qq.com>
2021-07-01 21:31:09 +0800

    waitting modifying

--- test_main.c
+++ test_main.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
 
 void bubblesort(int k[], int n);
 int main()
 {	
-	int *nums = (int *)malloc(sizeof(int));
-	int count, num;
+	int *nums;
+	int count=0, num;
+	char s;
 	printf("enter the list of number:\n");
 	while(scanf("%d", &num)!=EOF)
 	{
 		count++;
-		nums=(int *)realloc(count * sizeof(int));
+		nums=(int *)realloc(nums, count * sizeof(int));
 		nums[count-1]=num;
 		s=getchar();
 		if(s=='\n')
 			break;
 	}
 	printf("the list you entered is:\n");
-	for(int i=0;i<n-1;i++)
+	for(int i=0;i<count-1;i++)
 	{
 		printf("%d", nums[i]);
 	}
 	bubblesort(nums, count);
 	printf("sorted list is as follow:\n");
-	for(int i=0;i<n-1;i++)
+	for(int i=0;i<count-1;i++)
 	{
 		printf("%d", nums[i]);
 	}
