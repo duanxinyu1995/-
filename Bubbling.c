09a4b377f1eeaf53f1a7692a0cf40001d22fc7c0
duanxinyu1995 <1261102301@qq.com>
2021-07-03 15:01:38 +0800

    update

--- Bubbling.c
+++ Bubbling.c
@@ -1,10 +1,11 @@
+
 void bubblesort(int k[], int n)
 {
-	int i, j, temp;
-	for(i=0;i<n-1;i++)
+	int flag=0, temp;
+	for(int i=0;i<n-1;i++)
 	{
-		int flag=0;
-		for(j=0;j<=n-i;j++)
+		flag=0;
+		for(int j=0;j<n-i-1;j++)
 		{
 			if(k[j]>k[j+1])
 			{
@@ -12,7 +13,7 @@ void bubblesort(int k[], int n)
 				k[j+1]=k[j];
 				k[j]=temp;
 				flag=1;
-			}			
+			}
 		}
 		if(flag==0)
 			break;
--- sort.h
+++ sort.h
@@ -1,7 +1,4 @@
-#ifdef __SORT_H__
-#define __SORT_H__
+
 
 void bubblesort(int k[], int n);
 void quicksort(int k[], int s, int t);
-
-#endif
